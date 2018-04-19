c11953b6f82220950ddaf40db95bb3d481403ee0
Tyrn <orlov@bos-ltd.ru>
2018-04-19 23:41:22 +0300

    lists app moved to superlists

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from superlists.lists import views as lsviews
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # ~ url(r'^admin/', admin.site.urls),
+    url(r'^$', lsviews.home_page, name='home')
 ]
